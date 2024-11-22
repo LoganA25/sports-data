e3bb2fd905b6e51f3b9c337cca96098443761b04
Logan Allen <logandallen25@gmail.com>
2024-11-22 04:00:26 -0600

    Sensor works, need to add more to it.

--- sports/assets/players.py
+++ sports/assets/players.py
@@ -1,18 +1,24 @@
-from dagster import asset, define_asset_job
+from dagster import asset, define_asset_job, Config, DynamicPartitionsDefinition
+from typing import List
 from io import BytesIO
 
 import requests
 import pandas as pd
 
-@asset(group_name='teams', compute_kind='Python', io_manager_key='motherduck', key_prefix='teams')
-def players(context) -> pd.DataFrame:
+class Dates(Config):
+    week: str
+
+players_partitions = DynamicPartitionsDefinition(name="players")
+
+@asset(metadata={"partition_expr": "week"}, partitions_def=players_partitions, group_name="teams", compute_kind="Python", io_manager_key="motherduck", key_prefix="teams")
+def players(config: Dates) -> pd.DataFrame:
 
     postitions = ["QB", "WR", "RB", "TE", "K", "DB", "LB"]
 
     all_players = []
 
     for postition in postitions:
-        url = f"https://raw.githubusercontent.com/hvpkod/NFL-Data/main/NFL-data-Players/2024/1/{postition}.json"
+        url = f"https://raw.githubusercontent.com/hvpkod/NFL-Data/main/NFL-data-Players/2024/{config.week}/{postition}.json"
 
         file = BytesIO(requests.get(url).content)
 
@@ -21,6 +27,8 @@ def players(context) -> pd.DataFrame:
         all_players.append(df)
 
     df = pd.concat(all_players)
+
+    df['week'] = config.week
 
     return df
 
--- sports/sensors/players.py
+++ sports/sensors/players.py
@@ -1,16 +1,41 @@
-from dagster import schedule, ScheduleEvaluationContext, RunRequest
+from dagster import sensor, SensorEvaluationContext, SensorResult, RunRequest
 from sports.assets.players import players_job
+from sports.assets.players import players_partitions
 
+import requests
 
-@schedule(
-    job=players_job, cron_schedule="0 1 * * *", execution_timezone="US/Central"
-)
-def office_users(context: ScheduleEvaluationContext):
-    """
-    This schedule function triggers the 'players_job' to run daily at 1:00AM CST.
-    """
-    scheduled_date = context.scheduled_execution_time.strftime("%Y-%m-%d")
-    return RunRequest(
-        run_key=None,
-        tags={"date": scheduled_date},
-    )
+@sensor(job=players_job, name="players", minimum_interval_seconds=60)
+def players(context: SensorEvaluationContext):
+
+    positions = ["QB", "WR", "RB", "TE", "K", "DB", "LB"]
+
+    weeks = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18"]
+
+    for week in weeks:
+        week_found = False
+
+        for position in positions:
+            url = (f"https://raw.githubusercontent.com/hvpkod/NFL-Data/main/NFL-data-Players/2024/{week}/{position}.json")
+
+            try:
+                if requests.get(url).status_code == 200:
+                    context.log.info(f"Found week: {week} for position: {position}")
+                    week_found = True
+            
+                    return SensorResult(
+                        run_requests = [RunRequest(
+                        run_key=week,
+                        run_config={"ops": {"teams__players": {"config": {"week": week}}}},
+                        tags={"dagster/partition": week})
+                        ],
+                        dynamic_partitions_requests=[players_partitions.build_add_request([week])]
+                    )
+                else:
+                    context.log.info(f"Did not find week: {week} for position: {position}")
+
+            except Exception as e:
+                context.log.error(f"Error fetching week: {e}")
+
+        if not week_found:
+            context.log.info(f"Did not find week: {week} for any position")
+            break
